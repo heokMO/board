beaf63f31efe1f03aaa4aadcf2151f0bb46a561b
heokMO <gurah1220@gmail.com>
2023-07-28 00:40:34 +0900

    LoginAspect thread unsafty issue 수정

--- src/main/java/com/example/board/aspect/LoginAspect.java
+++ src/main/java/com/example/board/aspect/LoginAspect.java
@@ -1,9 +1,6 @@
 package com.example.board.aspect;
 
 import com.example.board.exception.CustomException;
-import com.example.board.exception.ExceptionMessage;
-import com.example.board.service.UserService;
-import com.example.board.util.CookieEncryptionUtil;
 import com.example.board.util.LoginChecker;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,28 +8,25 @@ import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Aspect
 @Component
 public class LoginAspect {
     private static final Logger log = LoggerFactory.getLogger(LoginAspect.class);
-    private final HttpServletRequest httpServletRequest;
     private final LoginChecker loginChecker;
 
-    public LoginAspect(HttpServletRequest httpServletRequest, LoginChecker loginChecker) {
-        this.httpServletRequest = httpServletRequest;
+    public LoginAspect(LoginChecker loginChecker) {
         this.loginChecker = loginChecker;
     }
 
 
     @Before("@annotation(LoginRequired)")
     public void checkLoginStatus(JoinPoint joinPoint) throws CustomException {
+        HttpServletRequest httpServletRequest = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         loginChecker.check(httpServletRequest);
     }
 }
--- src/main/java/com/example/board/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/board/controller/GlobalExceptionHandler.java
@@ -3,16 +3,26 @@ package com.example.board.controller;
 import com.example.board.dto.common.Message;
 import com.example.board.exception.CustomException;
 import com.example.board.exception.ExceptionMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<Message> handleCustomException(CustomException e){
         ExceptionMessage exceptionMessage = e.getExceptionMessage();
         return ResponseEntity.status(exceptionMessage.getHttpStatus()).body(Message.getErrorMessage(e));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Message>handleUnCatchException(Exception e){
+        logger.error("UnCatchException.", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Message.builder().message(e.getMessage()).build());
     }
 }
